bebb4ddfdd2e52f0eb52022878f4bb9cb53046b5
Richsvon <RichardLejon@hotmail.com>
2021-04-23 14:47:27 +0200

    dsg

---
+++ src/DbConnect.java
@@ -0,0 +1,22 @@
+
+import javafx.scene.control.Alert;
+
+import java.sql.*;
+
+public class DbConnect {
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public DbConnect() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://den1.mysql1.gear.host", "lab2se1", "Vi98?n9LS-4m");
+        } catch (SQLException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Failed to connect to the database");
+            alert.showAndWait();
+        }
+    }
+}
